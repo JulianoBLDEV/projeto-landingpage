f225b952cc989f5792f3480c1842ad018c69d492
JulianoBLDEV <julianobldeveloper@gmail.com>
2024-10-18 21:39:47 -0300

    Optimizing Javascript

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-const q1 = document.getElementById('q1');
-const s1 = document.getElementById('s1');
+const questions = document.querySelectorAll('.question'); /*Seleciona todos os objetos com a classe 'question' */
 
-function toggleanswer1(){
-    const a1 = document.getElementById('a1');
-    a1.classList.toggle('active');
-    s1.classList.toggle('active');
+questions.forEach(question => { /*Irá executar o código em cada elemento que possue a classe 'question'*/
+    const questionNumber = question.getAttribute('data-question'); /*Para cada elemento com a classe 'question' iremos pegar o valor de atributo do 'data-question' e armazenar na constante 'questionNumber'*/ 
+    const answer = document.querySelector(`.answer[data-answer="${questionNumber}"]`); /* Irá procurar em todos os elementos do documento que possui a classe 'answer' e a 'data-answer' que corresponde a 'questionNumber' e irá armazenas na constante 'answer'*/
+    const symbol = question.querySelector('.symbol'); /*Irá selecionar o elemento com classe 'symbol' e armazenar na constante 'symbol'*/
 
-}
-q1.addEventListener('click', toggleanswer1);
-s1.addEventListener('click', toggleanswer1);
-/*-----------------------------------------------------*/
-const q2 = document.getElementById('q2');
-const s2 = document.getElementById('s2');
+    function toggleAnswer() { /*criar alternância, caso não tenha a classe 'active' nos elementos com classe 'answer' e 'symbol' ele adiciona, se já possuir, ele retira*/
+        answer.classList.toggle('active');
+        symbol.classList.toggle('active');
+    }
 
-function toggleanswer2(){
-    const a2 = document.getElementById('a2');
-    a2.classList.toggle('active');
-    s2.classList.toggle('active');
-}
-q2.addEventListener('click', toggleanswer2);
-s2.addEventListener('click', toggleanswer2);
-/*-----------------------------------------------------*/
-const q3 = document.getElementById('q3');
-const s3 = document.getElementById('s3');
-
-function toggleanswer3(){
-    const a3 = document.getElementById('a3');
-    a3.classList.toggle('active');
-    s3.classList.toggle('active');
-}
-q3.addEventListener('click', toggleanswer3);
-s3.addEventListener('click', toggleanswer3);
-/*-----------------------------------------------------*/
-const q4 = document.getElementById('q4');
-const s4 = document.getElementById('s4');
-
-function toggleanswer4(){
-    const a4 = document.getElementById('a4');
-    a4.classList.toggle('active');
-    s4.classList.toggle('active');
-}
-q4.addEventListener('click', toggleanswer4);
-s4.addEventListener('click', toggleanswer4);
-/*-----------------------------------------------------*/
+    question.addEventListener('click', toggleAnswer); /*Chama a função 'toggleAnswer' ao clicar no elemento que possui a classe 'question'*/
+    symbol.addEventListener('click', toggleAnswer);/*Chama a função 'toggleAnswer' ao clicar no elemento que possui a classe 'symbol'*/
+});
